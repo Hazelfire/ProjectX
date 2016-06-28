8d49cf3151b85cd87e864fddc099fce7a653f8d8
Sam Nolan <samnolan555@gmail.com>
2016-06-28 16:34:01 +1000

    Add Parser for basic config

--- Classes/YAML/ConfigParser.cpp
+++ Classes/YAML/ConfigParser.cpp
@@ -3,8 +3,19 @@
 
 ConfigValues ConfigParser::parse(std::string script) {
 	YAML::Node root = YAML::Load(script.c_str());
-	for (YAML::const_iterator currentSection = root.begin(); currentSection != root.end(); currentSection++) {
-		std::string sectionName =  currentSection->first.as<std::string>();
-	}
-	return ConfigValues();
+	ConfigValues re;
+
+	if(root["chunkWidth"])
+		re.chunkWidth = root["chunkWidth"].as<int>();
+
+	if(root["chunkHeight"])
+		re.chunkHeight = root["chunkHeight"].as<int>();	
+
+	if(root["tileWidth"])
+		re.tileWidth = root["tileWidth"].as<int>();
+
+	if(root["tileHeight"])
+		re.tileHeight = root["tileHeight"].as<int>();
+
+	return re;
 }
--- Classes/YAML/ConfigParser.h
+++ Classes/YAML/ConfigParser.h
@@ -1,6 +1,9 @@
 #include <string>
 struct ConfigValues{
-
+	int chunkWidth = 20;
+	int chunkHeight = 20;
+	int tileHeight = 24;
+	int tileWidth = 32;
 };
 
 class ConfigParser {
